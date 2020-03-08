265f037486d1e4ffc00b91408a53b0d93e52230c
Aymeric Dominique <aymeric.dominique@gmail.com>
2020-03-08 17:57:43 +0100

    arrival ok

--- src/app/arrival/arrival.component.ts
+++ src/app/arrival/arrival.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { StationStatus } from '../services/api.service';
+import { Store, select } from '@ngrx/store';
+import { AppState } from '../reducers';
+import { currentPosition } from '../reducers/position';
+import { stationsStatus } from '../reducers/station-status';
+import { isLoading } from '../reducers/station-info';
+import { filter, take } from 'rxjs/operators';
+import { fetchingClosestStationsStatus } from '../actions/station-status';
 
 @Component({
   selector: 'app-arrival',
@@ -6,10 +15,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./arrival.component.scss']
 })
 export class ArrivalComponent implements OnInit {
+  currentPosition$: Observable<{ lat: number; lng: number }>;
+  stationsStatus$: Observable<StationStatus[]>;
+  isLoading$: Observable<boolean>;
 
-  constructor() { }
+  constructor(
+    private store: Store<AppState>
+  ) {
+    this.currentPosition$ = store.pipe(select(currentPosition));
+    this.stationsStatus$ = store.pipe(select(stationsStatus));
+    this.isLoading$ = store.pipe(select(isLoading));
+  }
 
   ngOnInit(): void {
+    this.currentPosition$
+      .pipe(filter(Boolean), take(1))
+      .subscribe((position: { lat: number; lng: number }) => {
+        this.store.dispatch(fetchingClosestStationsStatus(position));
+      });
   }
 
 }
