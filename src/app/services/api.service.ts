a71d9673e4634a59f8477e49b53abdcb03312dd8
Aymeric Dominique <aymeric.dominique@gmail.com>
2020-03-11 19:31:39 +0100

    https://velibetter.herokuapp.com

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -8,7 +8,7 @@ import { Coordinate, Station } from '../interfaces';
   providedIn: 'root'
 })
 export class ApiService {
-  private baseURL = 'https://velibetter.fr';
+  private baseURL = 'https://velibetter.herokuapp.com';
   constructor(private httpClient: HttpClient) {}
 
   fetchClosestInfo(
