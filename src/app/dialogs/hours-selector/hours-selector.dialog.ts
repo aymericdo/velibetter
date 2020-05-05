790c58fe749f3fb61e3b40a4270c84134725cc6a
Aymeric Dominique <aymeric.dominique@gmail.com>
2020-05-05 20:38:40 +0200

    edit vibrate number

--- src/app/dialogs/hours-selector/hours-selector.dialog.ts
+++ src/app/dialogs/hours-selector/hours-selector.dialog.ts
@@ -33,13 +33,13 @@ export class HoursSelectorComponent {
       this.scrollDelta += ((event.target as HTMLElement).scrollTop % 49 - this.precedentScrollTop % 49);
 
       if (this.precedentScrollDelta > this.scrollDelta) {
-        window.navigator.vibrate(100);
+        window.navigator.vibrate(30);
       }
     } else {
       this.scrollDelta -= (this.precedentScrollTop % 49 - (event.target as HTMLElement).scrollTop % 49);
 
       if (this.precedentScrollDelta < this.scrollDelta) {
-        window.navigator.vibrate(100);
+        window.navigator.vibrate(30);
       }
     }
 
