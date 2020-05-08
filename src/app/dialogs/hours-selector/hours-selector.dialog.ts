cb3dcae93c1ee5a2fced207bc424d05cdbb03697
Aymeric Dominique <aymeric.dominique@gmail.com>
2020-05-08 18:54:58 +0200

    each new hour, change the hour delta :princess:

--- src/app/dialogs/hours-selector/hours-selector.dialog.ts
+++ src/app/dialogs/hours-selector/hours-selector.dialog.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
 import { MatBottomSheetRef } from '@angular/material/bottom-sheet';
 import * as moment from 'moment';
 
@@ -7,19 +7,34 @@ import * as moment from 'moment';
   templateUrl: './hours-selector.dialog.html',
   styleUrls: ['./hours-selector.dialog.scss']
 })
-export class HoursSelectorComponent {
+export class HoursSelectorComponent implements OnInit, OnDestroy {
   @Output() deltaChanged: EventEmitter<number> = new EventEmitter<number>();
   hoverHour = null;
 
+  private interval: NodeJS.Timeout;
   private baseArray: [number, string][] = this.addRealHour(Array.from(Array(100).keys()).slice(1));
 
   hours: [number, string][] = [...this.baseArray];
 
   constructor(private bottomSheetRef: MatBottomSheetRef<HoursSelectorComponent>) { }
 
+  ngOnInit() {
+    this.interval = setInterval(() => {
+      const mins = new Date().getMinutes().toString();
+      if (mins === '00') {
+        this.hours = this.hours.map(([h, ]) => [h, this.getRealHour(h)]);
+      }
+      console.log(mins);
+    }, 60000);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.interval);
+  }
+
   addNextBatch(): void {
     const lastHour: number = this.hours[this.hours.length - 1][0];
-    this.baseArray.forEach(([h,]) => {
+    this.baseArray.forEach(([h, ]) => {
       this.hours.push([lastHour + h, this.getRealHour(lastHour + h)]);
     });
   }
@@ -29,7 +44,7 @@ export class HoursSelectorComponent {
     this.bottomSheetRef.dismiss();
   }
 
-  getRealHour(delta: number): string {
+  private getRealHour(delta: number): string {
     return moment().isSame(moment().add(delta, 'hour'), 'day') ?
         moment().add(delta + 1, 'hour').format('HH:00')
       :
